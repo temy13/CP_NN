5a68055f0b93aa61fa6e962992b0c65eb5566ad1
takigawa <takigawa@fast03.yama.info.waseda.ac.jp>
2016-12-22 13:05:01 +0900

    threading

--- nn.py
+++ nn.py
@@ -8,6 +8,8 @@ import chainer.functions as F
 import chainer.links as L
 from chainer import optimizers
 
+import threading
+
 #入力ベクトルが1000, 出力は2値
 class ClassificationModel(chainer.FunctionSet):
     #中間層は2つ。計４層
@@ -62,32 +64,59 @@ def set_optimizer(opt):
     print "error"
     return None
 
+
+def imple(d, h1_unit, h2_unit, opt):
+    
+    train_data = d["train_data"]
+    train_label = d["train_label"]
+    test_data = d["test_data"]
+    test_label = d["test_label"]
+
+    model = ClassificationModel(h1_unit, h2_unit)
+    #optimizer = optimizers.Adam()
+    optimizer = set_optimizer(opt)
+    optimizer.setup(model)
+
+    for epoch in range(500):
+      #勾配を0初期化
+      model.zerograds()
+      #学習,及び誤差関数の結果と精度の出力
+      loss, acc = model(train_data, train_label, train=True)
+      #逆伝搬による最適化
+      loss.backward()
+      optimizer.update()
+      #print epoch, "acc  ", acc.data
+
+    acc = model(test_data, test_label, train=False)
+    print opt, h1_unit, h2_unit, "acc test ", acc.data
+    model = optimizer = None
+
+
+class MyThread(threading.Thread):
+  def __init__(self, opts, d):
+      threading.Thread.__init__(self)
+      self.opts = opts
+      self.d = d
+                    
+  def run(self):
+      for opt in self.opts:  
+          for h1_unit in [n * 100 for n in range(1, 10)] + [10, 50]:
+          #for h1_unit in [100]:
+              for h2_unit in [n * 100 for n in range(1, 10)] + [10, 50]:
+              #for h2_unit in [50]:
+                  imple(self.d, h1_unit, h2_unit, opt)
+          
+
+
 def nn(train_label, train_data, test_label, test_data):
-    train_data = train_data.astype(np.float32)
-    train_label = train_label.astype(np.int32)
-    test_data = test_data.astype(np.float32)
-    test_label = test_label.astype(np.int32)
-    #for h1_unit in [n * 100 for n in range(1, 10)] + [10, 50]:
-    for h1_unit in [100]:
-        #for h2_unit in [n * 100 for n in range(1, 10)] + [10, 50]:
-        for h2_unit in [50]:
-            #for opt in ["SGD", "MomentumSGD", "AdaGrad", "AdaDelta", "Adam", "NesterovAG", "RMSprop", "RMSpropGraves", "SMORMS3"]:
-            for opt in ["AdaGrad"]:
-                model = ClassificationModel(h1_unit, h2_unit)
-                #optimizer = optimizers.Adam()
-                optimizer = set_optimizer(opt)
-                optimizer.setup(model)
-
-                for epoch in range(500):
-                    #勾配を0初期化
-                    model.zerograds()
-                    #学習,及び誤差関数の結果と精度の出力
-                    loss, acc = model(train_data, train_label, train=True)
-                    #逆伝搬による最適化
-                    loss.backward()
-                    optimizer.update()
-                    print epoch, "acc  ", acc.data
-
-                acc = model(test_data, test_label, train=False)
-                print opt, h1_unit, h2_unit, "acc test ", acc.data
-                model = optimizer = None
+    d = {}
+    d["train_data"]= train_data.astype(np.float32)
+    d["train_label"] = train_label.astype(np.int32)
+    d["test_data"] = test_data.astype(np.float32)
+    d["test_label"] = test_label.astype(np.int32)
+    ao = ["SGD", "MomentumSGD", "AdaGrad", "AdaDelta", "Adam", "NesterovAG", "RMSprop", "RMSpropGraves", "SMORMS3"]
+    #for opts in [ao[0:2], ao[2:4], ao[4:6], ao[6:7], ao[7:8], ao[8:]]:
+    #for opt in ["AdaGrad"]:
+    for opts in [ao[2:4] ]:
+      t = MyThread(opts, d)
+      t.start()
